602280b6e1e6fc7a2dd673c81bc950a04952acce
guusdias <gustavohd34@gmail.com>
2024-09-19 00:19:59 -0300

    feat: setup backend integration with front

--- frontend/src/api/course.ts
+++ frontend/src/api/course.ts
@@ -1,10 +1,57 @@
 import axios from "axios";
+import ICourse from "../types/ICourse"; // Certifique-se de que o caminho está correto para o tipo ICourse
 
 class Api {
-  static getApiData() {
+  static baseUrl = import.meta.env.VITE_API_URL;
+
+  static async getApiData() {
     return axios
-      .get(import.meta.env.VITE_API_URL)
-      .then((response) => response.data);
+      .get(Api.baseUrl)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Erro ao obter cursos:", error);
+        throw error;
+      });
+  }
+
+  static async getCourse(id: number) {
+    return axios
+      .get(`${Api.baseUrl}/${id}`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error(`Erro ao obter curso com ID ${id}:`, error);
+        throw error;
+      });
+  }
+
+  static async createCourse(course: ICourse) {
+    return axios
+      .post(Api.baseUrl, course)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Erro ao criar curso:", error);
+        throw error;
+      });
+  }
+
+  static async updateCourse(id: number, course: ICourse) {
+    return axios
+      .put(`${Api.baseUrl}/${id}`, course)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error(`Erro ao atualizar curso com ID ${id}:`, error);
+        throw error;
+      });
+  }
+
+  static async deleteCourse(id: number) {
+    return axios
+      .delete(`${Api.baseUrl}/${id}`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error(`Erro ao deletar curso com ID ${id}:`, error);
+        throw error;
+      });
   }
 }
 
