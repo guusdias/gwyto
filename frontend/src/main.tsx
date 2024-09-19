2cde90801d8c49bdafb537c5f51f9a167dcf23dc
guusdias <gustavohd34@gmail.com>
2024-09-19 00:29:52 -0300

    feat: setup global theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { colors } from "./global/theme.ts";
 import App from "./App.tsx";
 
+const theme = extendTheme({ colors });
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
